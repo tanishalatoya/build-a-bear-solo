5d808faec259663e9b9a1c4fb86fee63bcc0eb71
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-17 15:45:14 -0700

    Write comments to guide process of instantiating outfit objects on page load

--- main.js
+++ main.js
@@ -2,8 +2,17 @@ var saveButton = document.querySelector('#save-button');
 var backgroundButtons = document.querySelector('.backgrounds');
 var allGarments = [];
 
+window.addEventListener('load', instantiateOufits);
 saveButton.addEventListener('click', saveOutfit);
 backgroundButtons.addEventListener('click', assignBackgroundChoice)
+
+function instantiateOufits() {
+  //Get outfits from local localStorage --> localStorage.getItem --> should return an array of string elements
+  //Parse items back into objects --> JSON.Parse --> This should return an array of uninstantiated objects
+  //Loop over the objects within the array to reinstantiate each property of the outfit object as an instance
+  //Push reinstantiated outfit back into the allGarment array
+
+}
 
 function saveOutfit() {
   var outfitNameInput = document.querySelector('input');
