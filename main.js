fa111af6df30545d321f3fa64d485f1887d9659b
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-17 12:00:16 -0700

    Add function to select and assign background choice

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 var saveButton = document.querySelector('#save-button');
+var backgroundButtons = document.querySelector('.backgrounds');
 
 saveButton.addEventListener('click', saveOutfit);
+backgroundButtons.addEventListener('click', assignBackgroundChoice)
 
 function saveOutfit() {
   var outfitNameInput = document.querySelector('input');
   //Target the background and have the assigned value appear here
-
+  backgroundChoice = localStorage.getItem('background');
   //Use a uniqueID generator and target that
   uniqueID = generateUniqueID();
   //Instantiate a new outfit
   var outfit = new Outfit(outfitNameInput.value, backgroundChoice, uniqueID)
-
+  console.log(outfit);
 }
 
 function generateUniqueID() {
   return Math.floor(Math.random() * 10);
+}
+
+function assignBackgroundChoice() {
+  var backgroundSelection = event.target.id;
+  localStorage.setItem('background', backgroundSelection);
 }
