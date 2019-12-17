0ae461b50dfdc98ae74a67e08142cb860bd8be5a
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-17 15:50:47 -0700

    Add local variables to handle getting and parsing items from storage

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ backgroundButtons.addEventListener('click', assignBackgroundChoice)
 
 function instantiateOufits() {
   //Get outfits from local localStorage --> localStorage.getItem --> should return an array of string elements
+  var retrievedOutfits = localStorage('savedOutfits');
   //Parse items back into objects --> JSON.Parse --> This should return an array of uninstantiated objects
+  var parsedRetrievedOutfits = JSON.parse(retrievedOutfits);
   //Loop over the objects within the array to reinstantiate each property of the outfit object as an instance
+  for (var i = 0; i < parsedRetrievedOutfits.length; i++) {
+    //the code that creates the cards go here.
+  }
   //Push reinstantiated outfit back into the allGarment array
-
+  allGarments.push()
 }
 
 function saveOutfit() {
