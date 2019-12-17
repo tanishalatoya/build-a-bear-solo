1afa5846db880ed216f7e2e7b5c7f4cb2e916443
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-17 11:27:46 -0700

    Add key-value pairs to outfit constructor

--- outfit.js
+++ outfit.js
@@ -1,5 +1,8 @@
 class Outfit {
-  constructor() {
-    
+  constructor(title, background, id) {
+    this.title = title;
+    this.background = background || 'none';
+    this.id = id || undefined;
+    this.garments = [];
   }
 }
