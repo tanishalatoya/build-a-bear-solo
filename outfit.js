548876de2be6e1c0659e8c1de41f4b6561a9bfdf
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-17 14:50:42 -0700

    Update object with comments about notes for future changes to object

--- outfit.js
+++ outfit.js
@@ -7,12 +7,14 @@ class Outfit {
   }
 
   addGarment(attire) {
-    this.garments.push(attire);
+    allGarments.push(attire);
+    // 'attire' will be the object instance created on the main.js
   }
 
   removeGarment() {
+    console.log(event);
     for (var i = 0; i < this.garments.length; i++) {
-      //check for match of items) {
+      if (event.target.id === this.garments[i]) //'event.target.id' will need to be updated to something else as objects are passed into the array.
         this.garments.splice(i, 1);
       }
     }
