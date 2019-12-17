ea34a22b18d8defe1b660c12349a9b5e05f93e5e
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-17 11:50:48 -0700

    Instantiate a new outfit within saveOutfit function

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ var saveButton = document.querySelector('#save-button');
 saveButton.addEventListener('click', saveOutfit);
 
 function saveOutfit() {
+  var outfitNameInput = document.querySelector('input');
+  //Target the background and have the assigned value appear here
 
+  //Use a uniqueID generator and target that
+  uniqueID = generateUniqueID();
+  //Instantiate a new outfit
+  var outfit = new Outfit(outfitNameInput.value, backgroundChoice, uniqueID)
 
 }
